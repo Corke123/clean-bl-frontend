3aa87659a91af577783129bf8f41616a7e56d310
Corke123 <bojancoric996@gmail.com>
2021-08-06 22:28:32 +0200

    Enable sorting and filtering reports
    Add inputs to enable user to filter reports by title and by user created. Enable filtering by selecting status from dropdown menu.
    Enable sorting by creation date. Move paginator at the bottom of the page

--- src/app/modules/user/reports/report-list/report-list.component.ts
+++ src/app/modules/user/reports/report-list/report-list.component.ts
@@ -6,9 +6,19 @@ import {
   ViewChild,
 } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { merge, of } from 'rxjs';
-import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { MatSelect } from '@angular/material/select';
+import { BehaviorSubject, merge, of } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith,
+  switchMap,
+} from 'rxjs/operators';
+import { ReportStatus } from 'src/app/shared/model/report-status.model';
 import { CommonService } from 'src/app/shared/services/common.service';
+import { ReportStatusService } from 'src/app/shared/services/report-status.service';
 import { ReportService } from 'src/app/shared/services/reports.service';
 import { Report } from '../../../../shared/model/report.model';
 
@@ -19,27 +29,60 @@ import { Report } from '../../../../shared/model/report.model';
 })
 export class ReportListComponent implements OnInit, AfterViewInit {
   reports: Report[] = [];
+  statuses: ReportStatus[];
   totalElements: number;
+  sortLabel = 'Najnovije';
+  sortBy = 'createdAt';
+  sortOrder = new BehaviorSubject<string>('DESC');
+  title = new BehaviorSubject<string>('');
+  user = new BehaviorSubject<string>('');
+  activeFilters = 0;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSelect) statusSelect: MatSelect;
 
   constructor(
     private reportService: ReportService,
+    private reportStatusService: ReportStatusService,
     private commonService: CommonService,
     private changeDetection: ChangeDetectorRef
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.reportStatusService.getReportStatuses().subscribe(
+      (statuses: ReportStatus[]) => {
+        this.statuses = statuses;
+      },
+      () => {
+        this.commonService.showSnackBar(
+          'Došlo je do greške, nije moguće dobiti informacije o statusima'
+        );
+      }
+    );
+  }
 
   ngAfterViewInit(): void {
-    merge(this.paginator.page)
+    merge(
+      this.paginator.page,
+      this.sortOrder,
+      this.statusSelect.selectionChange,
+      this.title,
+      this.user
+    )
       .pipe(
         startWith({}),
+        debounceTime(300),
+        distinctUntilChanged(),
         switchMap(() => {
-          return this.reportService.getReports(
-            this.paginator.pageIndex,
-            this.paginator.pageSize
-          );
+          return this.reportService.getReports({
+            pageNumber: this.paginator.pageIndex,
+            pageSize: this.paginator.pageSize,
+            sortBy: this.sortBy,
+            sortDirection: this.sortOrder.getValue(),
+            status: this.statusSelect.value || '',
+            title: this.title.getValue(),
+            username: this.user.getValue(),
+          });
         }),
         map((page) => {
           this.totalElements = page.totalElements;
@@ -58,5 +101,30 @@ export class ReportListComponent implements OnInit, AfterViewInit {
     this.commonService.showSnackBar(
       'Greška, nije moguće dobiti informacije o prijavama!'
     );
+  }
+
+  onSort(ascending: boolean): void {
+    this.resetPaging();
+    if (ascending) {
+      this.sortLabel = 'Najstarije';
+      this.sortOrder.next('ASC');
+    } else {
+      this.sortLabel = 'Najnovije';
+      this.sortOrder.next('DESC');
+    }
+  }
+
+  resetPaging(): void {
+    this.paginator.pageIndex = 0;
+  }
+
+  onTitleChanged(input: string) {
+    this.resetPaging();
+    this.title.next(input);
+  }
+
+  onUserChanged(input: string) {
+    this.resetPaging();
+    this.user.next(input);
   }
 }

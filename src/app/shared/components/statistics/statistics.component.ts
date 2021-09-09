12517db9cc7347ce43bdc06355fbc2c150179d02
Corke123 <bojancoric996@gmail.com>
2021-09-09 21:05:01 +0200

    Split statistics on two tab panes

--- src/app/shared/components/statistics/statistics.component.ts
+++ src/app/shared/components/statistics/statistics.component.ts
@@ -6,14 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.css'],
 })
 export class StatisticsComponent implements OnInit {
-  selectedYear = new Date().getFullYear();
-  years: number[] = [];
-
   constructor() {}
 
-  ngOnInit(): void {
-    for (let i = 0; i < 3; i++) {
-      this.years[i] = this.selectedYear - i;
-    }
-  }
+  ngOnInit(): void {}
 }
